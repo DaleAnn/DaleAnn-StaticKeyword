66d3e1a7cc47e6a0ff6d9f9ea0769be8835cb2fa
DaleAnn <daviddaleann@yahoo.com>
2023-02-07 20:23:19 +0000

    Done!

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -22,6 +22,9 @@ public class Runner {
      * @param in the String value you should change global_var to in.
      */
     public void changeClassVar(String in){
+        Class cl = new Class();
+        cl.global_var = in;
+
 
     }
 }
